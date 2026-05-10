4bd13c90e2e72eaa371b865263ccd592d15f3964
dmitry-serov <frickmen@yandex.ru>
2026-05-11 00:28:15 +0300

    feat: add request and error logging

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,11 +2,13 @@ import 'dotenv/config';
 import path from 'path';
 import express from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import mongoose from 'mongoose';
 
 import { DB_ADDRESS, PORT } from './config';
 import NotFoundError from './errors/not-found-error';
 import errorHandler from './middlewares/error-handler';
+import { errorLogger, requestLogger } from './middlewares/logger';
 import orderRouter from './routes/orders';
 import productRouter from './routes/products';
 
@@ -15,6 +17,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(requestLogger);
 
 app.use('/product', productRouter);
 app.use('/order', orderRouter);
@@ -23,6 +26,8 @@ app.use((_req, _res, next) => {
   next(new NotFoundError('Маршрут не найден'));
 });
 
+app.use(errorLogger);
+app.use(errors());
 app.use(errorHandler);
 
 mongoose.connect(DB_ADDRESS);
