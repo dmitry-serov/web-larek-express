57ad22101c09a6f78b0148a6f300637f156a7661
dmitry-serov <frickmen@yandex.ru>
2026-05-10 20:19:04 +0300

    feat: connect mongodb and add config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,15 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
-const { PORT = 3000 } = process.env;
+import { DB_ADDRESS, PORT } from './config';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(DB_ADDRESS);
 
 app.listen(PORT);
